de1fc4634cb95f8ef62e5c6a77c84390a84db458
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-18 17:26:55 +0300

    generate variables, constructor, getters, setters and equals and hashcode for users object

--- src/main/java/models/Users.java
+++ src/main/java/models/Users.java
@@ -1,4 +1,71 @@
 package models;
 
+import java.util.Objects;
+
 public class Users {
+    private  int id;
+    private String name;
+    private String position;
+    private String email;
+    private String departmentId;
+
+    public Users(String name, String position, String email, String departmentId) {
+        this.name = name;
+        this.position = position;
+        this.email = email;
+        this.departmentId = departmentId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Users)) return false;
+        Users users = (Users) o;
+        return getId() == users.getId() && getName().equals(users.getName()) && getPosition().equals(users.getPosition()) && getEmail().equals(users.getEmail()) && getDepartmentId().equals(users.getDepartmentId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getPosition(), getEmail(), getDepartmentId());
+    }
 }
