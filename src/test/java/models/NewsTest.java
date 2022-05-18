88836088bc4171f5fb81fc13c140814e3b51880d
Sylvia-Chepkoech <slycheche@gmail.com>
2022-05-18 18:58:59 +0300

    add tests to news model

--- src/test/java/models/NewsTest.java
+++ src/test/java/models/NewsTest.java
@@ -1,7 +1,28 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class NewsTest {
 
+    public News newNews (){
+        return new News("Rarua", "Raila chooses Karua as running mate", 4);
+    }
+
+    @Test
+    public void getHeading_returnsHeadingCorrectly_String() {
+        News news = newNews();
+        assertEquals("Rarua", news.getHeading());
+    }
+
+    @Test
+    public void getContext_returnsContextCorrectly_String() {
+        News news = newNews();
+        assertEquals("Raila chooses Karua as running mate", news.getContext());
+    }@Test
+    public void getDepartmentId_returnsDepartmentIdCorrectly_int() {
+        News news = newNews();
+        assertEquals(4, news.getDepartmentId());
+    }
 }
